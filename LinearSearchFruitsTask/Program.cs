3a2394506f93650c61250d5a8d1d9c30613ccc2d
Tinh Nguyen <22TNguyen@heckgrammar.co.uk>
2025-05-14 12:45:56 +0100

    wfawefa

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -10,9 +10,25 @@
             // Ask the user what word they would like to find
             // Output the message True if the word is found
             // Output the message False if the word is not found
-            
+
             // You must write your own linear search routine and not use any built-in fuctions available in C#
             // You should use meaningful variable names
+
+            Console.WriteLine("Please input the item you would like to search for.");
+            string Search = Console.ReadLine();
+
+            //int i = 0;
+            bool found = false;
+            foreach (string j in fruits)
+            {
+                if (j == Search)
+                {
+                    found = true;
+                }
+                //i++;
+            }
+
+            Console.WriteLine(found);
         }
     }
 }
